656f7bae08abc440fbfe215162a3f63c415d22d0
junyojeo <jjy8479@gmail.com>
2024-02-09 05:08:38 +0900

    feat(pages): Add login page
    - Added a new login page to the application
    - The login page contains a form for users to enter their email address and password
    - Added buttons for signing in with Google, Facebook, and Twitter accounts
    - Included the necessary HTML and CSS code for the login page
    
    closes #123

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { Route } from '../lib/router/router.js';
 import { SetComponent } from '../lib/router/setcomponent.js';
 import { Navigate } from '../lib/router/navigate.js';
 // pages
-import { pageLogin } from './pages/login.js';
+import { pageLogIn } from './pages/login.js';
 import { pageProfile } from './pages/profile.js';
 import { pageGame } from './pages/game.js';
 import { pageTournament } from './pages/tournament.js';
@@ -13,33 +13,36 @@ import { sidebar } from './components/sidebar.js';
 import { profile } from './components/profile.js';
 import { getBoard, setBoard } from './components/pong.js';
 // state
-import { store, updateUI } from '../lib/state/store.js';
+import { store } from '../lib/state/store.js';
+import { updateProfile } from '../lib/state/update.js';
 import { checkLogin } from '../lib/state/check_login.js';
 import { defaultProfile } from '../lib/state/default_profile.js';
 
 // { 경로: { 이름, 페이지, 컴포넌트 } } 렌더링 될 component는 여러개일 수 있기에 배열로 설정
 const routes = {
-  '/login': { name: 'Login', page: pageLogin, component: [] },
+  '/login': { name: 'Login', page: pageLogIn, component: [] },
   '/profile': { name: 'Profile', page: pageProfile, component: [] },
   '/game': { name: 'Game', page: pageGame, component: [] },
   '/tournament': { name: 'Tournament', page: pageTournament, component: [] },
   '/logout': { name: 'Logout', page: pageSwitch, component: [] },
 };
 
-// SetComponent -> routes 객체의 component 배열에 속성 추가
-SetComponent(routes, sidebar(routes), profile('junyojeo'));
-// 나머지 페이지에도 컴포넌트 추가
-Route(routes);
-
-// 상태 변경을 구독하고, 상태가 변경될 때마다 updateUI 함수를 실행
-// 상태가 변경될 때마다 구독자(updateUI 함수를 뜻함)에게 알림을 보내는 역할
-store.subscribe(updateUI);
-
 function init() {
   window.onload = function () {
     // window.onload -> 브라우저가 새로고침 될 때마다 실행
-    checkLogin(store, routes);
-    defaultProfile();
+    // SetComponent -> routes 객체의 모든 속성에 component 속성을 추가
+    Route(routes);
+    if (window.location.pathname === '/') {
+      SetComponent(routes);
+    } else {
+      SetComponent(routes, sidebar(routes), profile('junyojeo'));
+      // defaultProfile -> 프로필 정보가 없을 때 기본 프로필을 생성
+      defaultProfile();
+    }
+    // store.subscribe() -> 상태가 변경될 때마다 실행
+    store.subscribe(updateProfile);
+    // checkLogin -> 로그인 상태 확인
+    checkLogin(routes);
   };
 
   // window.addEventListener() -> 브라우저의 이벤트를 수신하는 함수
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,15 @@
-export function pageLogin() {
+export function pageLogIn() {
   const page = document.createElement('div');
   page.setAttribute('class', 'full-screen');
+  // const content = `
+  //   <button id="loginButton">로그인</button>
+  //   <button id="logoutButton" style="display:none;">로그아웃</button>
+  // `;
   const content = `
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
-    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-      ...
-    </svg>
-
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-      ...
-    </div>
-
     <main class="form-signin w-100 m-auto">
       <form>
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
         <div class="form-floating">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
           <label for="floatingInput">Email address</label>
@@ -23,24 +18,14 @@ export function pageLogin() {
           <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
           <label for="floatingPassword">Password</label>
         </div>
-
-        <div class="form-check text-start my-3">
-          <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-          <label class="form-check-label" for="flexCheckDefault">
-            Remember me
-          </label>
+        <div class="form-check my-0.8">
         </div>
         <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-        <div class="oauth-container">
-          <button class="btn btn-google w-100 py-2 my-2" type="button">Sign in with Google</button>
-          <button class="btn btn-facebook w-100 py-2 my-2" type="button">Sign in with Facebook</button>
-          <button class="btn btn-twitter w-100 py-2 my-2" type="button">Sign in with Twitter</button>
-        </div>
       </form>
     </main>
     <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
-		  `;
+  	  `;
   page.innerHTML = content;
   return page;
 }
