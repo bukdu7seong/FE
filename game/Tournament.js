cf376c5078fd2414019df520defefdc1fe8f534f
lsh <salee2@student.42seoul.kr>
2024-02-10 16:17:40 +0900

    refactor: rename

--- game/Tournament.js
+++ game/Tournament.js
@@ -1,53 +1,55 @@
 import PingPong from './PingPong.js';
 
+const GameStage = {
+  SEMI_A: 1,
+  SEMI_B: 2,
+  FINAL: 3
+};
+
 export default class Tournament {
-  constructor(mode, players) {
+  constructor(mode, playerNames) {
     this.mode = mode;
-    this.players = players;
+    this.playerNames = playerNames;
     this.finalist = [];
-    this.stage = 'semi1';
+    this.stage = GameStage.SEMI_A;
+    this.semiFinalA = null;
+    this.semiFinalB = null;
+    this.final = null;
   }
 
   play() {
-    if (this.stage === 'semi1') {
-      if (!this.semiFinal1) {
-        this.semiFinal1 = new PingPong(this.mode, this.players[0], this.players[1]);
-        this.semiFinal1.onGameEnd = () => this.play();
-        this.semiFinal1.gameStart();
+    if (this.stage === GameStage.SEMI_A) {
+      if (!this.semiFinalA) {
+        this.semiFinalA = new PingPong(this.mode, this.playerNames[0], this.playerNames[1]);
+        this.semiFinalA.onGameEnd = () => this.play();
+        this.semiFinalA.gameStart();
       }
-      if (this.semiFinal1.gameState === 'end') {
-        this.finalist.push(this.semiFinal1.winner);
-        this.semiFinal1.ball.init();
-        delete this.semiFinal1;
-        this.stage = 'semi2';
-        alert('sem1 end');
+      if (this.semiFinalA.gameState === 'end') {
+        this.finalist.push(this.semiFinalA.winner);
+        this.stage = GameStage.SEMI_B;
+        alert('semiA end');
       }
     }
-    if (this.stage === 'semi2') {
-      if (!this.semiFinal2) {
-        this.semiFinal2 = new PingPong(this.mode, this.players[2], this.players[3]);
-        this.semiFinal2.player1.updateScore();
-        this.semiFinal2.player2.updateScore();
-        this.semiFinal2.onGameEnd = () => this.play();
-        this.semiFinal2.gameStart();
+    if (this.stage === GameStage.SEMI_B) {
+      if (!this.semiFinalB) {
+        this.semiFinalB = new PingPong(this.mode, this.playerNames[2], this.playerNames[3]);
+        this.semiFinalB.onGameEnd = () => this.play();
+        this.semiFinalB.gameStart();
       }
-      if (this.semiFinal2.gameState === 'end') {
-        this.finalist.push(this.semiFinal2.winner);
-        delete this.semiFinal2;
-        this.stage = 'final';
-        alert('sem2 end');
+      if (this.semiFinalB.gameState === 'end') {
+        this.finalist.push(this.semiFinalB.winner);
+        this.stage = GameStage.FINAL;
+        alert('semiB end');
       }
-      if (this.stage === 'final') {
-        if (!this.final) {
-          this.final = new PingPong(this.mode, this.finalist[0], this.finalist[1]);
-          this.final.player1.updateScore();
-          this.final.player2.updateScore();
-          this.final.onGameEnd = () => this.play();
-          this.final.gameStart();
-        }
-        if (this.final.gameState === 'end') {
-          alert('final end');
-        }
+    }
+    if (this.stage === GameStage.FINAL) {
+      if (!this.final) {
+        this.final = new PingPong(this.mode, this.finalist[0], this.finalist[1]);
+        this.final.onGameEnd = () => this.play();
+        this.final.gameStart();
+      }
+      if (this.final.gameState === 'end') {
+        alert('final end');
       }
     }
   }
