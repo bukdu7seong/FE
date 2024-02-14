799128815debb141f9d514f81091c99a1a76fac8
sideseal <ckgun@naver.com>
2024-02-15 01:11:29 +0900

    modify: modify render, route, state ...

--- src/utils/icon_status.js
+++ src/utils/icon_status.js
@@ -1,6 +1,8 @@
 // 이 함수는 페이지의 아이콘을 녹색으로 변경.
 export function setPageIconBackground(page) {
-  const icon = document.querySelector(`.${page}-icon`);
+  console.log(page);
+  const icon = document.querySelector(`.icon-${page.toLowerCase()}`);
+  console.log(icon);
   if (icon) {
     icon.style.backgroundColor = 'green';
   }
