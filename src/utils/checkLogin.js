fef79ab853c1dcd7bd5fdc8ce258f3a9f20bbbfd
lsh <lshhi100@gmail.com>
2024-04-03 19:29:51 +0900

    feat: 42인증 리다이렉트 핸들러

--- src/utils/checkLogin.js
+++ src/utils/checkLogin.js
@@ -8,7 +8,7 @@ export async function checkLogin() {
     window.location.search.includes('code')
   ) {
     localStorage.setItem('code', window.location.search);
-    redirectRoute('/signup');
+    // redirectRoute('/signup');
     return;
   }
 
