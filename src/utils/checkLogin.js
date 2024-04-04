fef661b0f381769219b515bbdf132d315dba0288
lsh <lshhi100@gmail.com>
2024-04-04 14:29:02 +0900

    refactor: checkLogin route

--- src/utils/checkLogin.js
+++ src/utils/checkLogin.js
@@ -6,14 +6,11 @@ export async function checkLogin() {
   const accessToken = getCookie('accessToken') || null;
   const signUpCode = localStorage.getItem('code') || null;
 
-  if (signUpCode) {
-    redirectRoute('/signup');
-    return;
-  }
-
   // need to validate access token
   if (accessToken) {
     globalState.setState({ isLoggedIn: true });
+  } else if (signUpCode) {
+    redirectRoute('/oauth2-redirect');
   } else {
     globalState.setState({ isLoggedIn: false });
     redirectRoute('/login');
