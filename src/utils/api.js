0bbfd3ed78ad6a2e344ee98b36e03c95489b9c5a
sideseal <ckgun@naver.com>
2024-04-07 18:06:35 +0900

    change: change http to https

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const ACCOUNT_API_URL = 'http://localhost:6000';
-export const GAME_API_URL = 'http://localhost:6001';
+export const ACCOUNT_API_URL = 'https://localhost:6000';
+export const GAME_API_URL = 'https://localhost:6001';
