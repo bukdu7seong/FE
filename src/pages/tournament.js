5fce438fa038d94fa176a90024aa462a753ada0f
lsh <lshhi100@gmail.com>
2024-04-10 20:10:45 +0900

    refactor: divide

--- src/pages/tournament.js
+++ src/pages/tournament.js
@@ -8,7 +8,6 @@ export function pageTournament() {
   const navbar = sidebar(routes);
 
   page.innerHTML = `
-<!--        <div class="side-bar"></div>-->
         <div class="main-box">
           <div class="user-box"></div>
           <div class="game-box" id="tournament">
@@ -19,9 +18,21 @@ export function pageTournament() {
               <div class="divider"></div>
               <div class="player-option" id="tournament-player2">PLAYER 2</div>
             </div>
-            
-            <button type="button" id='tournamentBtn' class="btn" data-bs-toggle="modal" data-bs-target="#tournamentSettingModal"></button>
+          </div>
+        </div>
+      `;
+  page.appendChild(navbar);
+  page.appendChild(createTournamentSettingModal());
+  page.appendChild(createTournamentRoundModal());
+  page.appendChild(createTournamentWinnerModal());
+  setupTournamentEvents(page);
+  return page;
+}
 
+function createTournamentSettingModal() {
+  const modalContainer = document.createElement('div');
+
+  modalContainer.innerHTML = `
 <div class="modal fade" id="tournamentSettingModal" tabindex="-1" aria-labelledby="tournamentSettingModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content bg-dark text-white">
@@ -101,11 +112,15 @@ export function pageTournament() {
         </div>
     </div>
 </div>
+`;
+  return modalContainer;
+}
 
-          </div>
-        </div>
-        
-        <div class="modal fade" id="tournamentRoundModal" tabindex="-1" aria-labelledby="tournamentRoundModalLabel" aria-hidden="true">
+function createTournamentRoundModal() {
+  const modalContainer = document.createElement('div');
+
+  modalContainer.innerHTML = `
+  <div class="modal fade" id="tournamentRoundModal" tabindex="-1" aria-labelledby="tournamentRoundModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content bg-dark text-white">
             <div class="modal-header border-0">
@@ -132,14 +147,40 @@ export function pageTournament() {
         </div>
     </div>
 </div>
-
-        
-      `;
-  page.appendChild(navbar);
-  page.appendChild(createTournamentWinnerModal());
-  setupTournamentEvents(page);
-  return page;
+`;
+  return modalContainer;
 }
+
+function createTournamentWinnerModal() {
+  const modalContainer = document.createElement('div');
+  modalContainer.innerHTML = `
+    <div class="modal fade" id="tournamentWinnerModal" tabindex="-1" aria-labelledby="tournamentWinnerModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content bg-dark text-white">
+              <div class="modal-header border-0">
+                  <h5 class="modal-title" id="tournamentWinnerModalLabel">TOURNAMENT WINNER!</h5>
+                  <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body text-center">
+                  <h6 id='tournament_player'>Player</h6>
+                  <input type="text" class="form-control bg-secondary text-white mb-4" id="winner-name" readonly>
+              </div>
+              <div class="modal-footer border-0 justify-content-center">
+                  <button type="button" class="btn btn-lg btn-success" id="restartGameButton">OK</button>
+              </div>
+          </div>
+      </div>
+    </div>`;
+
+  modalContainer.querySelector("#restartGameButton").addEventListener("click", function() {
+    const modalElement = document.getElementById('tournamentWinnerModal');
+    const modalInstance = bootstrap.Modal.getInstance(modalElement);
+    modalInstance.hide();
+  });
+
+  return modalContainer;
+}
+
 
 export function setupTournamentEvents(page) {
   gameState.setState({ currentGameStatus: 'idle' });
@@ -199,44 +240,6 @@ export function updateTournamentRoundModalContent(round) {
   document.getElementById("round-content").innerHTML = i18next.t("round-content");
   document.getElementById("startRoundButton").innerHTML = i18next.t("startRoundButton");
 }
-
-function createTournamentWinnerModal() {
-  const tournamentWinnerModal = document.createElement('div');
-  tournamentWinnerModal.className = 'modal fade';
-  tournamentWinnerModal.id = 'tournamentWinnerModal';
-  tournamentWinnerModal.tabIndex = -1;
-  tournamentWinnerModal.setAttribute('aria-labelledby', 'tournamentWinnerModalLabel');
-  tournamentWinnerModal.setAttribute('aria-hidden', 'true');
-  tournamentWinnerModal.innerHTML = `
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content bg-dark text-white">
-                <div class="modal-header border-0">
-                    <h5 class="modal-title" id="tournamentWinnerModalLabel">TOURNAMENT WINNER!</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body text-center">
-                    <h6 id='tournament_player'>Player</h6>
-                    <input type="text" class="form-control bg-secondary text-white mb-4" id="winner-name" readonly>
-                </div>
-                <div class="modal-footer border-0 justify-content-center">
-                    <button type="button" class="btn btn-lg btn-success" id="restartGameButton">OK</button>
-                </div>
-            </div>
-        </div>`;
-
-
-  // 모달 인스턴스 생성
-  tournamentWinnerModal.querySelector("#restartGameButton").addEventListener("click", function() {
-    const modalElement = document.getElementById('tournamentWinnerModal');
-    const modalInstance = bootstrap.Modal.getInstance(modalElement);
-    modalInstance.hide();
-  });
-
-  return tournamentWinnerModal;
-}
-
-
-
 export function updateTournamentWinnerModal() {
   document.getElementById("tournamentWinnerModalLabel").innerHTML = i18next.t("tournamentWinnerModalLabel");
   document.getElementById('tournament_player').innerHTML = i18next.t('tournament_player');
