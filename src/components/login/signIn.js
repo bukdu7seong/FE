7d956abe57a108489aa3dbb045c26e8e83ae3453
sideseal <ckgun@naver.com>
2024-04-10 20:05:38 +0900

    fix: fix refresh cookie

--- src/components/login/signIn.js
+++ src/components/login/signIn.js
@@ -14,6 +14,7 @@ async function requestLogin(userInfo) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(userInfo),
+      credentials: 'include',
     });
 
     if (response.status === 200) {
