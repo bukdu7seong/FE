ad4e37720be163125bede50fab6340dbda83e524
lsh <lshhi100@gmail.com>
2024-04-10 21:22:40 +0900

    refactor: delete comment unsubscribe.js

--- src/components/profile/modal/unsubscribe.js
+++ src/components/profile/modal/unsubscribe.js
@@ -1,11 +1,9 @@
-// 필요한 모듈 또는 유틸리티 가져오기
 import { failureToast } from '../../common/toast/failure.js';
 import { ACCOUNT_API_URL } from '../../../utils/api.js';
 import { getAccessToken } from '../../../utils/token.js';
 import { toastSuccess } from '../../../utils/success.js';
 import { toastFail } from '../../../utils/fail.js';
 
-// 회원 탈퇴 확인 모달 HTML
 function confirmDeletionModalHTML(modalId, finalModalId) {
   return `
 
@@ -156,7 +154,6 @@ export class deleteUserModal {
       this.frontModalInstance._element.remove();
       return;
     }
-    // 한 번에 element가 동시에 생기기에, 모두 없애야 한다.
     this.frontModalInstance._element.remove();
     this.backModalInstance._element.remove();
   }
@@ -164,7 +161,6 @@ export class deleteUserModal {
   show() {
     this.updateModalContent();
     this.frontModalInstance.show();
-    // userState.setState()
   }
 
   async deleteUserAccount(password) {
