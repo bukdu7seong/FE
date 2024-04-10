5de34b15d43a0f397dfd81258ebc24f6e57a4840
lsh <lshhi100@gmail.com>
2024-04-10 20:56:01 +0900

    refactor: delete comment applyLanguageClassicSetting.js

--- src/components/language/applyLanguageClassicSetting.js
+++ src/components/language/applyLanguageClassicSetting.js
@@ -2,7 +2,7 @@ export default function applyLanguageClassicSetting() {
   const elementsToUpdate = {
     'gameSettingModalLabel': i18next.t('gameSettingModalLabel'),
     'player2-label': i18next.t('player2-label'),
-    'player-name': i18next.t('player-name'), // placeholder의 경우 추가 처리 필요
+    'player-name': i18next.t('player-name'),
     'mode': i18next.t('mode'),
     'normal-label': i18next.t('normal-label'),
     'speed-label': i18next.t('speed-label'),
@@ -14,7 +14,7 @@ export default function applyLanguageClassicSetting() {
     const element = document.getElementById(id);
     if (element) {
       if (id === 'player-name') {
-        element.placeholder = text; // placeholder의 경우 별도 처리
+        element.placeholder = text;
       } else {
         element.innerHTML = text;
       }
