558abdc9cae8487bad52c491a1aa92d2c4561df1
lsh <lshhi100@gmail.com>
2024-04-11 12:05:54 +0900

    refactor: delete comment

--- src/utils/success.js
+++ src/utils/success.js
@@ -1,7 +1,6 @@
 import { successToast } from '../components/common/toast/success.js';
 
 export function toastSuccess(messageKey) {
-  // messageKey는 번역 키입니다. 예: 'connectSuccess'
   const translatedMessage = i18next.t(messageKey);
 
   const toast = new successToast(translatedMessage);
