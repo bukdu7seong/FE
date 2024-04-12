768ab9e3b688160f08949ae3abf656d3f318c82c
lsh <salee2@student.42seoul.kr>
2024-04-12 10:54:59 +0900

    fix : innerHTML => textContent

--- src/components/language/applyLanguageTournamentSettingModal.js
+++ src/components/language/applyLanguageTournamentSettingModal.js
@@ -1,19 +1,19 @@
 export default function applyLanguageTournamentSettingModal() {
   const elementsToUpdate = {
-    'tournamentSettingModalLabel': { text: i18next.t('tournamentSettingModalLabel'), type: 'innerHTML' },
-    't-player1-label': { text: i18next.t('t-player1-label'), type: 'innerHTML' },
+    'tournamentSettingModalLabel': { text: i18next.t('tournamentSettingModalLabel'), type: 'textContent' },
+    't-player1-label': { text: i18next.t('t-player1-label'), type: 'textContent' },
     'player1-name': { text: i18next.t('player1-name'), type: 'placeholder' },
-    't-player2-label': { text: i18next.t('t-player2-label'), type: 'innerHTML' },
+    't-player2-label': { text: i18next.t('t-player2-label'), type: 'textContent' },
     'player2-name': { text: i18next.t('player2-name'), type: 'placeholder' },
-    't-player3-label': { text: i18next.t('t-player3-label'), type: 'innerHTML' },
+    't-player3-label': { text: i18next.t('t-player3-label'), type: 'textContent' },
     'player3-name': { text: i18next.t('player3-name'), type: 'placeholder' },
-    't-player4-label': { text: i18next.t('t-player4-label'), type: 'innerHTML' },
+    't-player4-label': { text: i18next.t('t-player4-label'), type: 'textContent' },
     'player4-name': { text: i18next.t('player4-name'), type: 'placeholder' },
-    't-mode': { text: i18next.t('t-mode'), type: 'innerHTML' },
-    't-normal-label': { text: i18next.t('t-normal-label'), type: 'innerHTML' },
-    't-speed-label': { text: i18next.t('t-speed-label'), type: 'innerHTML' },
-    't-object-label': { text: i18next.t('t-object-label'), type: 'innerHTML' },
-    'startTournamentButton': { text: i18next.t('startTournamentButton'), type: 'innerHTML' }
+    't-mode': { text: i18next.t('t-mode'), type: 'textContent' },
+    't-normal-label': { text: i18next.t('t-normal-label'), type: 'textContent' },
+    't-speed-label': { text: i18next.t('t-speed-label'), type: 'textContent' },
+    't-object-label': { text: i18next.t('t-object-label'), type: 'textContent' },
+    'startTournamentButton': { text: i18next.t('startTournamentButton'), type: 'textContent' }
   };
 
   for (const [id, { text, type }] of Object.entries(elementsToUpdate)) {
--- src/components/language/applyLanguageTournamentWinnerModal.js
+++ src/components/language/applyLanguageTournamentWinnerModal.js
@@ -1,8 +1,8 @@
 export default function applyLanguageTournamentWinnerModal() {
   const elementsToUpdate = {
-    'tournamentWinnerModalLabel': { text: i18next.t('tournamentWinnerModalLabel'), type: 'innerHTML' },
-    'tournament_player': { text: i18next.t('tournament_player'), type: 'innerHTML' },
-    'restartGameButton': { text: i18next.t('restartGameButton'), type: 'innerHTML' }
+    'tournamentWinnerModalLabel': { text: i18next.t('tournamentWinnerModalLabel'), type: 'textContent' },
+    'tournament_player': { text: i18next.t('tournament_player'), type: 'textContent' },
+    'restartGameButton': { text: i18next.t('restartGameButton'), type: 'textContent' }
   };
 
   for (const [id, { text, type }] of Object.entries(elementsToUpdate)) {
